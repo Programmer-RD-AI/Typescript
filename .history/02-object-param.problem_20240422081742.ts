48ab2d585a984c1769c33d08749409e44e18dc73
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-04-22 09:22:13 +0530

    Initial commit

---
+++ .history/02-object-param.problem_20240422081742.ts
@@ -0,0 +1,22 @@
+import { expect, it } from "vitest";
+
+type NewType = { first: number; second: number };
+export const addTwoNumbers = (params: NewType) => {
+  return params.first + params.second;
+};
+
+it("Should add the two numbers together", () => {
+  expect(
+    addTwoNumbers({
+      first: 2,
+      second: 4,
+    })
+  ).toEqual(6);
+
+  expect(
+    addTwoNumbers({
+      first: 10,
+      second: 20,
+    })
+  ).toEqual(30);
+});
---
+++ .history/03-optional-properties.problem_20240422082224.ts
@@ -0,0 +1,27 @@
+import { expect, it } from "vitest";
+
+export const getName = (params: { first: string; last?: string }) => {
+  if (params.last) {
+    return `${params.first} ${params.last}`;
+  }
+  return params.first;
+};
+
+// Adding a ? next to a pararmeter 
+
+it("Should work with just the first name", () => {
+  const name = getName({
+    first: "Matt",
+  });
+
+  expect(name).toEqual("Matt");
+});
+
+it("Should work with the first and last name", () => {
+  const name = getName({
+    first: "Matt",
+    last: "Pocock",
+  });
+
+  expect(name).toEqual("Matt Pocock");
+});
